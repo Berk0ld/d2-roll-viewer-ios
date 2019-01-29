6b3f8b4b0501ea7e71ee14e6f261a9fde4de3275
Andrey Moskvin <andrey.moskvin@sigmaukraine.com>
2019-01-29 17:00:28 +0200

    Initial project generated from Xcode

---
+++ D2RollViewer/D2RollViewerTests/D2RollViewerTests.swift
@@ -0,0 +1,34 @@
+//
+//  D2RollViewerTests.swift
+//  D2RollViewerTests
+//
+//  Created by amoskvin on 1/28/19.
+//  Copyright © 2019 D2RollViewer. All rights reserved.
+//
+
+import XCTest
+@testable import D2RollViewer
+
+class D2RollViewerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
